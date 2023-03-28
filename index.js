7182b36354bbb69d2e6a2ba1a035ae294d2d3fe7
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-03-28 18:44:38 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(array){
+    for(let obj of array){
+        if(obj.result === "W"){
+            return obj.year;
+        }
+    }
+    return undefined;
+  }
